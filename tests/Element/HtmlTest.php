bb507ee90263fa24b513cf3e13f1cb1138efaa62
Ian Oswald <ian@ozzie.id.au>
2026-04-25 16:08:24 +1000

    chore(tests): add html tests for optional components

--- tests/Element/HtmlTest.php
+++ tests/Element/HtmlTest.php
@@ -34,3 +34,27 @@ test('new_element', function () {
     expect($result)->not->toBe($element);
     expect((string) $element)->toBe('<!DOCTYPE html><html><body><span></span></body></html>');
 });
+
+test('head_renders_when_populated', function () {
+    $element = new Html;
+    $element->head->add_content('<meta charset="utf-8">');
+    expect((string) $element)->toBe('<!DOCTYPE html><html><head><meta charset="utf-8"></head><body></body></html>');
+});
+
+test('title_renders_when_populated', function () {
+    $element = new Html;
+    $element->title->add_content('My Page');
+    expect((string) $element)->toBe('<!DOCTYPE html><html><head><title>My Page</title></head><body></body></html>');
+});
+
+test('noscript_renders_when_populated', function () {
+    $element = new Html;
+    $element->noscript->add_content('Please enable JavaScript.');
+    expect((string) $element)->toBe('<!DOCTYPE html><html><body><noscript>Please enable JavaScript.</noscript></body></html>');
+});
+
+test('doctype_customisation', function () {
+    $element = new Html;
+    $element->doctype = 'html PUBLIC "-//W3C//DTD HTML 4.01//EN"';
+    expect((string) $element)->toStartWith('<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN">');
+});
